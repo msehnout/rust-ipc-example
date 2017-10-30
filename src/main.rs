ab14dd083b7553a49753a33a4395685456bc4138
Sehny <msehnout@users.noreply.github.com>
2017-10-30 20:51:52 +0200

    Change reading loop

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
-use std::os::unix::net::{UnixStream, UnixListener};
+use std::os::unix::net::UnixListener;
 use std::io::{self, Read, Error, ErrorKind};
 use std::str;
 
 fn run_server() -> io::Result<()> {
+    let _ = std::fs::remove_file("/tmp/rusty-server");
     let listener = UnixListener::bind("/tmp/rusty-server")?;
     let (mut stream, _) = listener.accept()?;
     let mut buffer = vec![0u8; 512];
-    while let Ok(len) = stream.read(&mut buffer) {
-        if len != 0 {
-            println!("Received: {}", str::from_utf8(&buffer[..len])
-                .map_err(|_| Error::new(ErrorKind::Other,
-                                        "Input is not valid UTF-8"))?
-                .trim());
-        } else {
-            break;
+    loop {
+        match stream.read(&mut buffer){
+            Ok(0) => break,
+            Ok(len) => println!("Received: {}", str::from_utf8(&buffer[..len])
+                                .map_err(|_| Error::new(ErrorKind::Other, "Input is not valid UTF-8"))?
+                                .trim()),
+            Err(e) => return Err(e),
         }
     }
     drop(listener);
